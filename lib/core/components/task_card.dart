0cd1d74886c284fa989ece9bb9d7a256b4781468
mohamed hisham <mohamedhis36@gmail.com>
2025-03-05 12:13:14 +0200

    added gesture detector to the task_card.dart and optimized the text

--- lib/core/components/task_card.dart
+++ lib/core/components/task_card.dart
@@ -7,47 +7,62 @@ class TaskCard extends StatelessWidget {
     required this.image,
     required this.title,
     required this.reward,
+    required this.onTap,
   });
 
   final String image;
   final String title;
   final String reward;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.sizeOf(context).width > 700
         ? 700
         : MediaQuery.sizeOf(context).width;
-    return Material(
-      shape: RoundedRectangleBorder(
-        side: BorderSide(color: ColorManager.lightGrey, width: 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image(
-              image: AssetImage(image),
-              fit: BoxFit.fill,
-              width: screenWidth - 15,
-            ),
-          ),
-          Container(
-            height: 45,
-            decoration: BoxDecoration(
-              color: ColorManager.white.withOpacity(0.85),
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
-            ),
-            child: Center(
-              child: Text(
-                '$title            \$$reward',
-                style: TextStyle(fontSize: 16),
+    return GestureDetector(
+      onTap: onTap,
+      child: Material(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: ColorManager.lightGrey, width: 1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image(
+                image: AssetImage(image),
+                fit: BoxFit.fill,
+                width: screenWidth - 15,
               ),
             ),
-          )
-        ],
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                color: ColorManager.white.withOpacity(0.85),
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(10)),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      '\$$reward',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
